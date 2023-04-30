7d71f84af10f7ef99b750a727bce656cf67ead53
Célio V. Júnior <celio_cjr@hotmail.com>
2023-04-30 20:36:51 +1200

    fix the problem about the hover play button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
-import { Home as HomeIcon, SearchIcon, LibraryIcon, ChevronLeft, ChevronRight } from 'lucide-react'
+import { Home as HomeIcon, SearchIcon, LibraryIcon, ChevronLeft, ChevronRight, Play } from 'lucide-react'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -52,29 +52,49 @@ export default function Home() {
           <h1 className='font-semibold text-3xl mt-10'>Good Afternoon</h1>
 
           <div className="grid grid-cols-3 gap-4 mt-4">
-            <a href="#" className="bg-white/10 flex items-center gap-4 rounded overflow-hidden hover:bg-white/20 transition-colors">
+            <a href="#" className="bg-white/10 group flex items-center gap-4 rounded overflow-hidden hover:bg-white/20 transition-colors">
               <Image src="/cover.jpg" width={104} height={104} alt="Cover" />
               <strong>13th Floor Elevator</strong>
+
+              <button className='w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible'>
+                <Play />
+              </button>
+
             </a>
-            <a href="#" className="bg-white/10 flex items-center gap-4 rounded overflow-hidden hover:bg-white/20 transition-colors">
+            <a href="#" className="bg-white/10 group flex items-center gap-4 rounded overflow-hidden hover:bg-white/20 transition-colors">
               <Image src="/cover.jpg" width={104} height={104} alt="Cover" />
               <strong>13th Floor Elevator</strong>
+              <button className='w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible'>
+                <Play />
+              </button>
             </a>
-            <a href="#" className="bg-white/10 flex items-center gap-4 rounded overflow-hidden hover:bg-white/20 transition-colors">
+            <a href="#" className="bg-white/10 group flex items-center gap-4 rounded overflow-hidden hover:bg-white/20 transition-colors">
               <Image src="/cover.jpg" width={104} height={104} alt="Cover" />
               <strong>13th Floor Elevator</strong>
+              <button className='w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible'>
+                <Play />
+              </button>
             </a>
-            <a href="#" className="bg-white/10 flex items-center gap-4 rounded overflow-hidden hover:bg-white/20 transition-colors">
+            <a href="#" className="bg-white/10 group flex items-center gap-4 rounded overflow-hidden hover:bg-white/20 transition-colors">
               <Image src="/cover.jpg" width={104} height={104} alt="Cover" />
               <strong>13th Floor Elevator</strong>
+              <button className='w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible'>
+                <Play />
+              </button>
             </a>
-            <a href="#" className="bg-white/10 flex items-center gap-4 rounded overflow-hidden hover:bg-white/20 transition-colors">
+            <a href="#" className="bg-white/10 group flex items-center gap-4 rounded overflow-hidden hover:bg-white/20 transition-colors">
               <Image src="/cover.jpg" width={104} height={104} alt="Cover" />
               <strong>13th Floor Elevator</strong>
+              <button className='w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible'>
+                <Play />
+              </button>
             </a>
-            <a href="#" className="bg-white/10 flex items-center gap-4 rounded overflow-hidden hover:bg-white/20 transition-colors">
+            <a href="#" className="bg-white/10 group flex items-center gap-4 rounded overflow-hidden hover:bg-white/20 transition-colors">
               <Image src="/cover.jpg" width={104} height={104} alt="Cover" />
               <strong>13th Floor Elevator</strong>
+              <button className='w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible'>
+                <Play />
+              </button>
             </a>
           </div>
 

493a7bc1d8048c0c979ae78994c597de7d4d38f0
Célio V. Júnior <celio_cjr@hotmail.com>
2023-05-14 20:47:57 +1200

    screen finished

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
-import { Home as HomeIcon, SearchIcon, LibraryIcon, ChevronLeft, ChevronRight, Play, Shuffle, SkipBack, SkipForward, Repeat } from 'lucide-react'
+import { Home as HomeIcon, SearchIcon, LibraryIcon, ChevronLeft, ChevronRight, Play, Shuffle, SkipBack, SkipForward, Repeat, Mic2, LayoutList, Laptop2, Volume, Maximize2, Maximize } from 'lucide-react'
 
 const inter = Inter({ subsets: ['latin']})
 
@@ -130,8 +130,8 @@ export default function Home() {
 
         </main>
       </div>
-      <footer className='bg-zinc-800 border-t border-zinc-700 p-6 flex items-center justify-between'>
-        <div>
+      <footer className='bg-zinc-800 border-t border-zinc-700 px-6 py-4 flex items-center justify-between'>
+        <div className='flex items-center gap-3'>
           <Image src="/cover.jpg" width={56} height={56} alt="Cover" />
           <div className='flex flex-col'>
             <strong className='font-normal'>Music Name</strong>
@@ -151,14 +151,23 @@ export default function Home() {
           <div className='flex items-center gap-2'>
             <span className='text-xs text-zinc-400'>0:22</span>
               <div className='h-1 rounded-full w-96 bg-zinc-600'>
-                <div className='bg-zinc-200 w-40 h-1 rounded-full'>
-
-                </div>
+                <div className='bg-zinc-200 w-40 h-1 rounded-full'></div>
               </div>
             <span className='text-xs text-zinc-400'>2:49</span>
           </div>
         </div>
-        <div></div>
+        <div className='flex items-center gap-4'>
+          <Mic2 size={20} />
+          <LayoutList size={20} />
+          <Laptop2 size={20} />
+          <div className='flex items-center gap-2'>
+            <Volume size={20} />
+            <div className='h-1 rounded-full w-24 bg-zinc-600'>
+              <div className='bg-zinc-200 w-10 h-1 rounded-full'></div>
+            </div>
+          </div>
+          <Maximize2 size={20} />
+        </div>
       </footer>
     </div>
   )
